3a29bb7bca72613060d61830e3a2029dbf4a976c
Kenny Daniel <platypii@gmail.com>
2025-03-20 12:21:52 -0700

    Initial JS project

---
+++ test/package.test.js
@@ -0,0 +1,24 @@
+import { describe, expect, it } from 'vitest'
+import packageJson from '../package.json' with { type: 'json' }
+
+describe('package.json', () => {
+  it('should have the correct name', () => {
+    expect(packageJson.name).toBe('hyparquet-writer')
+  })
+  it('should have a valid version', () => {
+    expect(packageJson.version).toMatch(/^\d+\.\d+\.\d+$/)
+  })
+  it('should have MIT license', () => {
+    expect(packageJson.license).toBe('MIT')
+  })
+  it('should have precise dev dependency versions', () => {
+    const { devDependencies } = packageJson
+    Object.values(devDependencies).forEach(version => {
+      expect(version).toMatch(/^\d+\.\d+\.\d+$/)
+    })
+  })
+  it('should have no dependencies', () => {
+    expect('dependencies' in packageJson).toBe(false)
+    expect('peerDependencies' in packageJson).toBe(false)
+  })
+})
