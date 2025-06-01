37508f69aaa62bf1e6a9e8997601811334986dd4
Kenny Daniel <platypii@gmail.com>
2025-05-31 17:00:15 -0700

    Export types

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ export { parquetWrite, parquetWriteBuffer } from './write.js'
 export { autoSchemaElement, schemaFromColumnData } from './schema.js'
 export { ByteWriter } from './bytewriter.js'
 export { ParquetWriter } from './parquet-writer.js'
+
+/**
+ * @typedef {import('hyparquet').KeyValue} KeyValue
+ * @typedef {import('hyparquet').SchemaElement} SchemaElement
+ * @typedef {import('../src/types.d.ts').BasicType} BasicType
+ * @typedef {import('../src/types.d.ts').ColumnSource} ColumnSource
+ * @typedef {import('../src/types.d.ts').ParquetWriteOptions} ParquetWriteOptions
+ * @typedef {import('../src/types.d.ts').Writer} Writer
+ */
